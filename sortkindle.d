d8b8c187351bf108098f3dafcc191a70768ca91d
return5 <armoredmoose@gmail.com>
2020-11-28 11:34:48 -0600

    made get title more readable. title now removes characters unix pathfile doenst like

--- sortkindle.d
+++ sortkindle.d
@@ -11,12 +11,16 @@ import std.regex;
 import std.range;
 
 string getTitle(string title, string clip) {
+    auto remove  = ctRegex!("[\n|\r|\"|:|\']*","g");              //match to remove these chars from title
     if(clip.split(regex(" +")).length == 1) {
-        return title ~ "_defs.txt";
+        title = title ~ "_defs.txt";
     }
     else {
-        return title ~ "_clip.txt";
+        title = title ~ "_clip.txt";
     }
+    title = strip(title,"\357\273\277");
+    title = replaceAll(title,remove,"");
+    return title;
 }
 
 void main(string[] args) {
@@ -24,8 +28,7 @@ void main(string[] args) {
 	string clipping       = readText(args[1]);                         //clipping file to be read
     auto entire_part      = ctRegex!r"(.+[\r*|\n*]+)+?(==========)";  //gets entire clipping
 	auto title_part       = ctRegex!r"(.+)(\r|\n)*(- Highlight)";    //matches title
-	auto loc_part         = ctRegex!r"- Highlight Loc.\s+\d+\s*";  //matches location number.
-    auto remove           = ctRegex!("[\n|\r]*","g");              //match to remove these chars from title
+	auto loc_part         = ctRegex!r"- Highlight Loc.\s+\d+\s*";   //matches location number.
 
 	//go through text file and get text which matches entire_part regex 
 	foreach(line; matchAll(clipping,entire_part)) { 	
@@ -34,11 +37,11 @@ void main(string[] args) {
 			auto clip_match  = strip(line.captures[1]);                     //get clippiing
 			auto loc_match   = matchFirst(line.captures[0],loc_part);      //get loc.
 			if(title_match && clip_match && loc_match) {                  //if all three were matched successfully
-                string title     = getTitle(replaceAll(strip(title_match.captures[1],"\357\273\277"),remove,""),clip_match);
+                string title     = getTitle(title_match.captures[1],clip_match);
 				string clip      = strip(clip_match);
 				string location  = strip(loc_match.hit);
                 if((title in files) == null) {                   //if file doesnt exist for the title, create one in files array
-					files[title] = File(title,"w");
+					files[title] = File(strip(title),"w");
 				}
 				else {
 					files[title].writeln("\n",'='.repeat(12),"\n");  //write to file
