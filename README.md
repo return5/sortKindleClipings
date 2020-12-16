84f3b7cb1e20d8cb5cd48dcb15847a5e1141c191
return5 <armoredmoose@gmail.com>
2020-12-16 15:28:23 -0500

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# sortKindleClipings
+program for sorting through clippings made on an amazon kindle.
+
+takes text file as command line argument and sorts through it saving each cliping to a text file for the book which the clipping came from. 
+
+each book you make clipings from has its own individual .txt file where the clipings are saved to. 
---
+++ sortkindle.d
@@ -0,0 +1,53 @@
+//written by: github/return5
+//released under MIT license 
+//program which takes text file containing clippings from amazon kindle and seperates each clipping into a text file for the book which it came from.
+//seperates single words from multi words.  single words are assumed to be words to look up/save definition of. these are saved in file with post-fix "_def.txt"
+//lines with more than one word are savged in text file with post-fix "_clip.txt"
+
+import std.stdio;
+import std.file;
+import std.string;
+import std.regex;
+import std.range;
+
+string getTitle(string title, string clip) {
+    auto remove  = ctRegex!("[\n|\r|\"|:|\']*","g");              //match to remove these chars from title
+    if(clip.split(regex(" +")).length == 1) {
+        title = title ~ "_defs.txt";
+    }
+    else {
+        title = title ~ "_clip.txt";
+    }
+    title = strip(title,"\357\273\277");
+    title = replaceAll(title,remove,"");
+    return title;
+}
+
+void main(string[] args) {
+	File[string] files;                                                 //associative array of files
+	string clipping       = readText(args[1]);                         //clipping file to be read
+    auto entire_part      = ctRegex!r"(.+[\r*|\n*]+)+?(==========)";  //gets entire clipping
+	auto title_part       = ctRegex!r"(.+)(\r|\n)*(- Highlight)";    //matches title
+	auto loc_part         = ctRegex!r"- Highlight Loc.\s+\d+\s*";   //matches location number.
+
+	//go through text file and get text which matches entire_part regex 
+	foreach(line; matchAll(clipping,entire_part)) { 	
+		if(line) { //found a match
+			auto title_match = matchFirst(line.captures[0],title_part);      //get title
+			auto clip_match  = strip(line.captures[1]);                     //get clippiing
+			auto loc_match   = matchFirst(line.captures[0],loc_part);      //get loc.
+			if(title_match && clip_match && loc_match) {                  //if all three were matched successfully
+                string title     = getTitle(title_match.captures[1],clip_match);
+				string clip      = strip(clip_match);
+				string location  = strip(loc_match.hit);
+                if((title in files) == null) {                   //if file doesnt exist for the title, create one in files array
+					files[title] = File(strip(title),"w");
+				}
+				else {
+					files[title].writeln("\n",'='.repeat(12),"\n");  //write to file
+               	}
+               	files[title].write(clip,"\n\t ",location);
+			}
+		}
+	}
+}
